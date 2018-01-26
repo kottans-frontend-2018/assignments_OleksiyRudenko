f6bafe63ebd294f6e0af649f4d6e26455b4a5a88
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-01-27 01:39:41 +0200

    Add timeMs2Time helper function

--- task_12/js/helpers.js
+++ task_12/js/helpers.js
@@ -36,3 +36,15 @@ function getLocale() {
   else
     return navigator.language;
 }
+
+/**
+ * Converts milliseconds to minutes and seconds mm:SS
+ * @param milliseconds
+ * @returns {string} mm:SS
+ */
+function timeMs2Time(milliseconds) {
+  let seconds = Math.round(milliseconds/1000);
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+  return minutes + ':' + (seconds < 10 ? '0'+seconds : seconds);
+}
