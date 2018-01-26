2a921208a6f3462dbbec81950914f6be73eb1482
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-01-27 01:09:47 +0200

    Improve helper functions

--- task_12/js/helpers.js
+++ task_12/js/helpers.js
@@ -1,22 +1,35 @@
-function updateClock(clock) {
-  clock.innerHTML = new Date().toLocaleTimeString(getLang());
+/**
+ * Updates current time display
+ * @param clockElement
+ */
+function updateClock(clockElement) {
+  clockElement.innerHTML = new Date().toLocaleTimeString(getLocale());
 }
 
+/**
+ * Initializes clock display
+ */
 function initializeClock() {
   const currentTimeElement = document.getElementById("current-time");
   updateClock(currentTimeElement);
-  window.setInterval(() => {
-    updateClock(currentTimeElement);
-  }, 1000);
+  window.setInterval(() => updateClock(currentTimeElement),
+    1000);
 }
 
+/**
+ * Updates copyright year
+ */
 function setCopyYear() {
   const copyYearElement = document.getElementById("copy-year");
-  copyYearElement.innerText = new Date().getFullYear();
+  copyYearElement.innerText = '' + new Date().getFullYear();
 }
 
-function getLang() {
-  if (navigator.languages != undefined)
+/**
+ * Detects user agent locale
+ * @returns {string}
+ */
+function getLocale() {
+  if (navigator.languages !== undefined)
     return navigator.languages[0];
   else
     return navigator.language;
