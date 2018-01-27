b5d948a75cb108d65e77e0eea72b562753ae5445
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-01-27 02:25:06 +0200

    Make pizza timing more lifelike

--- task_12/js/class.PizzaSet.js
+++ task_12/js/class.PizzaSet.js
@@ -2,9 +2,10 @@ class PizzaSet {
   constructor(pizzaCount) {
     this.pizzaSet = [];
     let date = new Date();
+    date.setTime(date.getTime() - (4 * 60 + 50) * 1000 );
     for (let i=0; i < pizzaCount; i++) {
       date.setTime(date.getTime() + i*25*1000);
-      this.pizzaSet.push(new Pizza(i+1, date, 1));
+      this.pizzaSet.push(new Pizza(i+1, date, 5));
     }
     this.updatePizzasTiming();
     window.setInterval(() => this.updatePizzasTiming(),
