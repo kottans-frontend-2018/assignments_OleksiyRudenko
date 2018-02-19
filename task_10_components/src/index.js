41787ebe034c958350d72e8fca1fb9bf524022df
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-02-20 00:36:00 +0200

    feat(wwa-app): Bootstrap the app

--- task_10_components/src/index.js
+++ task_10_components/src/index.js
@@ -1,6 +1,10 @@
+import App from './components/app/App.js';
+
 /**
  * App JS entry point
  */
 const appContainer = document.getElementById(
   document.getElementById('entry-script').getAttribute('data-app-container')
 );
+
+window.app = new App(appContainer);
